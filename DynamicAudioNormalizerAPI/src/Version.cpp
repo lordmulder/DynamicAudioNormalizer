lordmulder/DynamicAudioNormalizer

commit 2789c919bd5848a1846a1ac3ebce4e4b1ec5f092

Added support for Visual Studio 2013 with Update 3.

DynamicAudioNormalizerAPI/src/Version.cpp
@@ -24,48 +24,45 @@
 //Version info
 const unsigned int DYNAUDNORM_VERSION_MAJOR = 2;
 const unsigned int DYNAUDNORM_VERSION_MINOR = 3;
-const unsigned int DYNAUDNORM_VERSION_PATCH = 4;
+const unsigned int DYNAUDNORM_VERSION_PATCH = 5;
 
 //Build date/time
 const char* DYNAUDNORM_BUILD_DATE = __DATE__;
 const char* DYNAUDNORM_BUILD_TIME = __TIME__;
 
 //Compiler detection
+//Compiler detection
 #if defined(__INTEL_COMPILER)
 	#if (__INTEL_COMPILER >= 1300)
-		static char *DYNAUDNORM_COMPILER = "ICL 13.x";
+		const char *DYNAUDNORM_COMPILER = "ICL 13." LAMEXP_MAKE_STR(__INTEL_COMPILER_BUILD_DATE);
 	#elif (__INTEL_COMPILER >= 1200)
-		static char *DYNAUDNORM_COMPILER = "ICL 12x.";
+		const char *DYNAUDNORM_COMPILER = "ICL 12." LAMEXP_MAKE_STR(__INTEL_COMPILER_BUILD_DATE);
 	#elif (__INTEL_COMPILER >= 1100)
-		static char *DYNAUDNORM_COMPILER = "ICL 11.x";
+		const char *DYNAUDNORM_COMPILER = "ICL 11.x";
 	#elif (__INTEL_COMPILER >= 1000)
-		static char *DYNAUDNORM_COMPILER = "ICL 10.x";
+		const char *DYNAUDNORM_COMPILER = "ICL 10.x";
 	#else
 		#error Compiler is not supported!
 	#endif
 #elif defined(_MSC_VER)
 	#if (_MSC_VER == 1800)
-		#if (_MSC_FULL_VER < 180021005)
-			const char *DYNAUDNORM_COMPILER = "MSVC 2013-Beta";
-		#elif (_MSC_FULL_VER < 180030501)
+		#if (_MSC_FULL_VER == 180021005)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2013";
 		#elif (_MSC_FULL_VER == 180030501)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2013.2";
+		#elif (_MSC_FULL_VER == 180030723)
+			const char *DYNAUDNORM_COMPILER = "MSVC 2013.3";
 		#else
 			#error Compiler version is not supported yet!
 		#endif
 	#elif (_MSC_VER == 1700)
-		#if (_MSC_FULL_VER < 170050727)
-			const char *DYNAUDNORM_COMPILER = "MSVC 2012-Beta";
-		#elif (_MSC_FULL_VER < 170051020)
-			const char *DYNAUDNORM_COMPILER = "MSVC 2012");
-		#elif (_MSC_FULL_VER < 170051106)
-			const char *DYNAUDNORM_COMPILER = "MSVC 2012.1-CTP";
-		#elif (_MSC_FULL_VER < 170060315)
+		#if (_MSC_FULL_VER == 170050727)
+			const char *DYNAUDNORM_COMPILER = "MSVC 2012";
+		#elif (_MSC_FULL_VER == 170051106)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2012.1";
-		#elif (_MSC_FULL_VER < 170060610)
+		#elif (_MSC_FULL_VER == 170060315)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2012.2";
-		#elif (_MSC_FULL_VER < 170061030)
+		#elif (_MSC_FULL_VER == 170060610)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2012.3";
 		#elif (_MSC_FULL_VER == 170061030)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2012.4";
@@ -73,12 +70,10 @@ const char* DYNAUDNORM_BUILD_TIME = __TIME__;
 			#error Compiler version is not supported yet!
 		#endif
 	#elif (_MSC_VER == 1600)
-		#if (_MSC_FULL_VER < 160040219)
-			const char *DYNAUDNORM_COMPILER = "MSVC 2010";
-		#elif (_MSC_FULL_VER == 160040219)
+		#if (_MSC_FULL_VER >= 160040219)
 			const char *DYNAUDNORM_COMPILER = "MSVC 2010-SP1";
 		#else
-			#error Compiler version is not supported yet!
+			const char *DYNAUDNORM_COMPILER = "MSVC 2010";
 		#endif
 	#elif (_MSC_VER == 1500)
 		#if (_MSC_FULL_VER >= 150030729)
@@ -94,14 +89,10 @@ const char* DYNAUDNORM_BUILD_TIME = __TIME__;
 	#if !defined(_M_X64) && defined(_M_IX86_FP)
 		#if (_M_IX86_FP == 1)
 			#pragma message("SSE instruction set is enabled!")
-		#elif (_M_IX86_FP == 2)
+		#elif (_M_IX86_FP >= 2)
 			#pragma message("SSE2 (or higher) instruction set is enabled!")
 		#endif
 	#endif
-#elif defined(__GNUC__)
-	#define GCC_VERSION_GLUE1(X,Y,Z) "GCC " #X "." #Y "." #Z
-	#define GCC_VERSION_GLUE2(X,Y,Z) GCC_VERSION_GLUE1(X,Y,Z)
-	const char *DYNAUDNORM_COMPILER = GCC_VERSION_GLUE2(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
 #else
 	#error Compiler is not supported!
 #endif

commit cfb1def526c4ed90bab761667dc45771393a50e8

Fixed GCC detection. Regression in 2789c919bd5848a1846a1ac3ebce4e4b1ec5f092.

DynamicAudioNormalizerAPI/src/Version.cpp
@@ -84,7 +84,6 @@ const char* DYNAUDNORM_BUILD_TIME = __TIME__;
 	#else
 		#error Compiler is not supported!
 	#endif
-
 	// Note: /arch:SSE and /arch:SSE2 are only available for the x86 platform
 	#if !defined(_M_X64) && defined(_M_IX86_FP)
 		#if (_M_IX86_FP == 1)
@@ -93,6 +92,10 @@ const char* DYNAUDNORM_BUILD_TIME = __TIME__;
 			#pragma message("SSE2 (or higher) instruction set is enabled!")
 		#endif
 	#endif
+#elif defined(__GNUC__)
+	#define GCC_VERSION_GLUE1(X,Y,Z) "GCC " #X "." #Y "." #Z
+	#define GCC_VERSION_GLUE2(X,Y,Z) GCC_VERSION_GLUE1(X,Y,Z)
+	const char *DYNAUDNORM_COMPILER = GCC_VERSION_GLUE2(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
 #else
 	#error Compiler is not supported!
 #endif