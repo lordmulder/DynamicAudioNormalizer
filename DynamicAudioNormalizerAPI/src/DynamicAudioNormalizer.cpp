d3e1b02195dd4ea0639278394f8de69a0a98e083
LoRd_MuldeR <mulder2@gmx.de>
2014-08-25 22:56:35 +0200

    Fixed compilation on CentOS.

--- DynamicAudioNormalizerAPI/src/DynamicAudioNormalizer.cpp
+++ DynamicAudioNormalizerAPI/src/DynamicAudioNormalizer.cpp
@@ -19,6 +19,10 @@
 // http://www.gnu.org/licenses/lgpl-2.1.txt
 //////////////////////////////////////////////////////////////////////////////////
 
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS 1
+#endif
+
 #include "DynamicAudioNormalizer.h"
 
 #include "Common.h"
