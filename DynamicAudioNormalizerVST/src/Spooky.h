b7e41661fd61c67551b24d5f6a22e8044d1e9452
LoRd_MuldeR <mulder2@gmx.de>
2014-09-14 19:26:11 +0200

    Spooky Hash: Added info where to find the original sources.

--- DynamicAudioNormalizerVST/src/Spooky.h
+++ DynamicAudioNormalizerVST/src/Spooky.h
@@ -1,6 +1,9 @@
 // Spooky Hash
 // A 128-bit noncryptographic hash, for checksums and table lookup
 // By Bob Jenkins.  Public domain.
+//
+// This is a stripped-down version derived from the original source code. Find the original code at:
+// http://burtleburtle.net/bob/hash/spooky.html
 
 #include <cstdlib>
 #include <cstdint>
