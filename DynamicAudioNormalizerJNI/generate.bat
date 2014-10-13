e114011e25efac73d9297b820622d73f7335df80
LoRd_MuldeR <mulder2@gmx.de>
2014-10-13 16:06:13 +0200

    Added JNI wrapper project.

---
+++ DynamicAudioNormalizerJNI/generate.bat
@@ -0,0 +1,11 @@
+@echo off
+
+if not exist "%JDK_PATH%\bin\javah.exe" (
+	echo Could not find 'javah.exe', please check your JDK_PATH environment variable!
+	pause & exit
+)
+
+mkdir "%~dp0\include" 2> NUL
+"%JDK_PATH%\bin\javah.exe" -d "%~dp0\include" -cp "%~dp0\bin" DynamicAudioNormalizer.JDynamicAudioNormalizer
+
+pause
---
+++ DynamicAudioNormalizerJNI/src/DynamicAudioNormalizer/JDynamicAudioNormalizer.java
@@ -0,0 +1,107 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Dynamic Audio Normalizer - Java/JNI Wrapper
+// Copyright (c) 2014 LoRd_MuldeR <mulder2@gmx.de>. Some rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// http://opensource.org/licenses/MIT
+//////////////////////////////////////////////////////////////////////////////////
+
+package DynamicAudioNormalizer;
+
+public class JDynamicAudioNormalizer
+{
+	//------------------------------------------------------------------------------------------------
+	// Exception class
+	//------------------------------------------------------------------------------------------------
+	
+	public static class Error extends RuntimeException
+	{
+		private static final long serialVersionUID = 8560360516304094422L;
+
+		public Error(String string)
+		{
+			super(string);
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	// Initialization
+	//------------------------------------------------------------------------------------------------
+
+	static
+	{
+		try
+		{
+			System.loadLibrary("DynamicAudioNormalizerAPI"); // Load native library at runtime
+		}
+		catch(UnsatisfiedLinkError e)
+		{
+			System.err.println(e.getMessage());
+			System.err.println("ERROR: Failed to load native DynamicAudioNormalizerAPI library!\n");
+			throw new Error("Failed to load DynamicAudioNormalizerAPI library");
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	// Native API
+	//------------------------------------------------------------------------------------------------
+	
+	private static class NativeAPI
+	{
+		private native static boolean getVersionInfo(int versionInfo[]);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	// Public API
+	//------------------------------------------------------------------------------------------------
+
+	static int[] getVersionInfo()
+	{
+		int[] versionInfo = new int[3];
+		
+		try
+		{
+			if(!NativeAPI.getVersionInfo(versionInfo))
+			{
+				throw new RuntimeException("Failed to retrieve version info from native library!");
+			}
+		}
+		catch(UnsatisfiedLinkError e)
+		{
+			System.err.println(e.getMessage());
+			System.err.println("ERROR: Failed to call native DynamicAudioNormalizerAPI function!\n");
+			throw new Error("Failed to call native function!");
+		}
+		
+		return versionInfo;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	// Test Functions
+	//------------------------------------------------------------------------------------------------
+	
+	public static void main(String[] args)
+	{
+		System.out.println("DynamicAudioNormalizer JNI Tester\n");
+	
+		int[] versionInfo = JDynamicAudioNormalizer.getVersionInfo();
+		System.out.println("Library Version: " + versionInfo[0] + "." + versionInfo[1] + "-" + versionInfo[2]);
+	}
+}
