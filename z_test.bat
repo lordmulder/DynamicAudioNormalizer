a7db97f26644f56f9aa985194224de8748e5ae78
LoRd_MuldeR <mulder2@gmx.de>
2015-01-11 21:19:28 +0100

    Improved the evaluation script.

--- z_test.bat
+++ z_test.bat
@@ -1,5 +1,8 @@
 @echo off
 
+set "WAV2PNG=D:\Temp\Downloads\wav2png.exe"
+set "CONVERT=D:\ImageMagick-6.9.0-Q16\convert.exe"
+
 rmdir /Q /S "%~dp0\tmp" 2> NUL
 mkdir "%~dp0\tmp"
 
@@ -28,6 +31,10 @@ for %%f in (%~dp0\test\*.wav) do (
 	echo ===========================================================================
 	echo.
 	"%~dp0\bin\Win32\Release_Static\DynamicAudioNormalizerCLI.exe" -i "%%~f" -o "%~dp0\tmp\%%~nf.%1.wav" -l "%~dp0\tmp\%%~nf.%1.log" %2 %3 %4 %5 %6 %7 %8 %9
+	"%WAV2PNG%" --width 1920 -o "%~dp0\tmp\~%%~nf.%1.A.png" "%%~f"
+	"%WAV2PNG%" --width 1920 -o "%~dp0\tmp\~%%~nf.%1.B.png" "%~dp0\tmp\%%~nf.%1.wav"
+	"%CONVERT%" "%~dp0\tmp\~%%~nf.%1.A.png" "%~dp0\tmp\~%%~nf.%1.B.png" -append "%~dp0\tmp\%%~nf.%1.png"
+	del "%~dp0\tmp\~%%~nf.%1.?.png"
 )
 
 echo.
