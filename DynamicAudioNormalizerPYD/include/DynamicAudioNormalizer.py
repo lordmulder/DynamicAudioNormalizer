39f063b000feeac77fd539686e1c55cb589effd1
LoRd_MuldeR <mulder2@gmx.de>
2017-02-18 00:19:34 +0100

    Python bindings: Implemented WaveWriter class for Python wrapper.

--- DynamicAudioNormalizerPYD/include/DynamicAudioNormalizer.py
+++ DynamicAudioNormalizerPYD/include/DynamicAudioNormalizer.py
@@ -47,37 +47,31 @@ class DynamicAudioNormalizer:
 		return "DynamicAudioNormalizer v{}.{}-{} [{}][{}]]".format(*versionInfo[0], buildStr, configStr)
 	
 	def __init__(self,channels,samplerate):
-		print("__init__")
 		self._channels = channels
 		self._samplerate = samplerate
 		self._instance = None
 	
 	def __enter__(self):
-		print("__enter__")
 		self._instance = DynamicAudioNormalizerAPI.create(self._channels, self._samplerate)
 		print(self._instance)
 		return self
 	
 	def __exit__(self, type, value, traceback):
-		print("__exit__")
 		if not self._instance:
 			raise RuntimeError("Instance not initialized!")
 		DynamicAudioNormalizerAPI.destroy(self._instance)
 		self._instance = None
 	
 	def __del__(self):
-		print("__del__")
 		if self._instance:
 			print("RESOURCE LEAK: DynamicAudioNormalizer object was not de-initialized properly!", file=sys.stderr)
 	
 	def getConfig(self):
-		print("getConfig()")
 		if not self._instance:
 			raise RuntimeError("Instance not initialized!")
 		return DynamicAudioNormalizerAPI.getConfig(self._instance)
 	
 	def processInplace(self, samplesInOut, count):
-		print("processInplace()")
 		if not self._instance:
 			raise RuntimeError("Instance not initialized!")
 		return DynamicAudioNormalizerAPI.processInplace(self._instance, samplesInOut, count)
@@ -96,11 +90,22 @@ class _WaveFileBase:
 		cidx, sidx = 0, 0
 		type = self._get_sample_type(sampwidth)
 		for sample in struct.iter_unpack(type[0], samples):
-			buffers[cidx][sidx] = sample[0] / type[1]
+			buffers[cidx][sidx] = float(sample[0]) / float(type[1])
 			cidx += 1
 			if cidx >= channels:
 				cidx, sidx = 0, sidx + 1
 		return sidx
+	
+	def _repack_samples(self, buffers, channels, sampwidth, length):
+		type = self._get_sample_type(sampwidth)
+		samples = b""
+		for sidx in range(0, length):
+			for cidx in range(0, channels):
+				if type[1] > 1.4142:
+					samples += struct.pack(type[0], int(round(buffers[cidx][sidx] * type[1])))
+				else:
+					samples += struct.pack(type[0], buffers[cidx][sidx])
+		return samples
 	
 	def _minimum_buff_length(self, buffers):
 		result = sys.maxsize
@@ -111,9 +116,7 @@ class _WaveFileBase:
 		return result
 	
 	def _get_sample_type(self, sampwidth):
-		if sampwidth == 1:
-			return ('B', 0xFF)
-		elif sampwidth == 2:
+		if sampwidth == 2:
 			return ('<h', 0x7FFF)
 		elif sampwidth == 4:
 			return ('<f', 1.0)
@@ -164,6 +167,11 @@ class WaveFileReader(_WaveFileBase):
 			raise RuntimeError("Instance not initialized!")
 		return self._samplerate
 		
+	def getSampleWidth(self):
+		if not self._wavefile:
+			raise RuntimeError("Instance not initialized!")
+		return self._samplewidth
+		
 	def read(self, buffers):
 		if not self._wavefile:
 			raise RuntimeError("Instance not initialized!")
@@ -172,6 +180,67 @@ class WaveFileReader(_WaveFileBase):
 		frames = self._wavefile.readframes(self._minimum_buff_length(buffers))
 		if frames:
 			return self._unpack_samples(frames, buffers, self._channels, self._samplewidth)
+		return None
+
+
+class WaveFileWriter(_WaveFileBase):
+	"""Helper class to write samples to Wave file"""
+	
+	def __init__(self,filename,channels,samplewidth,samplerate):
+		self._filename = filename
+		self._wavefile = None
+		self._channels = channels
+		self._samplewidth = samplewidth
+		self._samplerate = samplerate
+	
+	def __enter__(self):
+		self._wavefile = wave.open(self._filename, 'wb')
+		if self._wavefile:
+			self._wavefile.setnchannels(self._channels)
+			self._wavefile.setsampwidth(self._samplewidth)
+			self._wavefile.setframerate(self._samplerate)
+		return self
+	
+	def __exit__(self, type, value, traceback):
+		if not self._wavefile:
+			raise RuntimeError("Instance not initialized!")
+		self._wavefile.close()
+		self._wavefile = None
+		self._channels = None
+		self._samplewidth = None
+		self._samplerate = None
+	
+	def __del__(self):
+		if self._wavefile:
+			print("RESOURCE LEAK: WaveFileWriter object was not de-initialized properly!", file=sys.stderr)
+			
+	def getChannels(self):
+		if not self._wavefile:
+			raise RuntimeError("Instance not initialized!")
+		return self._channels
+		
+	def getSamplerate(self):
+		if not self._wavefile:
+			raise RuntimeError("Instance not initialized!")
+		return self._samplerate
+		
+	def getSampleWidth(self):
+		if not self._wavefile:
+			raise RuntimeError("Instance not initialized!")
+		return self._samplewidth
+		
+	def write(self, buffers, length):
+		if not self._wavefile:
+			raise RuntimeError("Instance not initialized!")
+		if len(buffers) < self._channels:
+			raise RuntimeError("Number of buffers is insufficient!")
+		if length < 1:
+			raise RuntimeError("The given length value is zero!")
+		if length > self._minimum_buff_length(buffers):
+			raise RuntimeError("Length exceeds size of buffer!")
+		samples = self._repack_samples(buffers, self._channels, self._samplewidth, length)
+		if samples:
+			return self._wavefile.writeframes(samples)
 		return None
 
 
@@ -193,15 +262,15 @@ def alloc_buffers(channles, length):
 # Main
 #----------------------------------------------------------------------
 
-print(DynamicAudioNormalizer)
 print(DynamicAudioNormalizer.getVersion())
 
-with WaveFileReader('The Root of All Evil.wav') as wav_reader:
-	with DynamicAudioNormalizer(wav_reader.getChannels(), wav_reader.getSamplerate()) as normalizer:
-		buffers = alloc_buffers(wav_reader.getChannels(), 4096)
-		while True:
-			count = wav_reader.read(buffers)
-			if count:
-				print(normalizer.processInplace(buffers, count))
-				continue
-			break;
+with WaveFileReader('The Answer Lies Within.wav') as wav_reader:
+	with WaveFileWriter('Output.wav', wav_reader.getChannels(), wav_reader.getSampleWidth(), wav_reader.getSamplerate()) as wav_writer:
+		with DynamicAudioNormalizer(wav_reader.getChannels(), wav_reader.getSamplerate()) as normalizer:
+			buffers = alloc_buffers(wav_reader.getChannels(), 4096)
+			while True:
+				count = wav_reader.read(buffers)
+				if count:
+					wav_writer.write(buffers, count)
+					continue
+				break;
